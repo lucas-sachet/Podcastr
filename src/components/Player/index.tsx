1df48f928c3cf9f9125b6b6c9510fbdcac0541d4
Lucas Sachet <lucasachet@gmail.com>
2021-04-20 15:01:04 -0300

    feat: create empty player

---
+++ src/components/Player/index.tsx
@@ -0,0 +1,44 @@
+import styles from './styles.module.scss'
+
+export function Player() {
+  return (
+   <div className={styles.playerContainer}>
+     <header>
+       <img src="/playing.svg" alt="playing now"/>
+       <strong>Playing now</strong>
+     </header>
+
+     <div className={styles.emptyPlayer}>
+       <strong>Select one podcast to play</strong>
+     </div>
+
+     <footer className={styles.empty}>
+       <div className={styles.progress}>
+         <span>00:00</span>
+         <div className={styles.slider}>
+          <div className={styles.emptySlider} />
+         </div>
+         <span>00:00</span>
+       </div>
+
+       <div className={styles.buttons}>
+         <button type="button">
+          <img src="/shuffle.svg" alt="shuffle"/>
+         </button>
+         <button type="button">
+          <img src="/play-previous.svg" alt="previous"/>
+         </button>
+         <button type="button" className={styles.playButton}>
+          <img src="/play.svg" alt="play"/>
+         </button>
+         <button type="button">
+          <img src="/play-next.svg" alt="next"/>
+         </button>
+         <button type="button">
+          <img src="/repeat.svg" alt="repeat"/>
+         </button>
+       </div>
+     </footer>
+   </div>
+  );
+}
