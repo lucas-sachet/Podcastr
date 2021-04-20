92ef6d20c0c4943fdabc575cbea6c13fc850cdfd
Lucas Sachet <lucasachet@gmail.com>
2021-04-20 14:14:41 -0300

    feat: create header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import format from 'date-fns/format'
+import enUS from 'date-fns/locale/en-US'
+
+import styles from './styles.module.scss'
+
+export function Header() {
+  const currentDate = format(new Date(), 'EEE, d MMMM', {
+    locale: enUS
+  })
+
+  return (
+    <header className={styles.headerContainer}>
+      <img src="/logo.svg" alt="Podcastr"/>
+
+      <p>The best for you to listen!</p>
+
+      <span>{currentDate}</span>
+    </header>
+  );
+}
