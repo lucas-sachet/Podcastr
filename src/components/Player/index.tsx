a5e324fc41819fa47e5504ebcd5fee6c17bde29e
Lucas Sachet <lucasachet@gmail.com>
2021-04-25 21:06:29 -0300

    feat: create player context

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,6 +1,16 @@
+import Image from 'next/image';
+import { useContext } from 'react';
+import { PlayerContext } from '../../contexts/PlayerContext';
+import Slider from 'rc-slider'
+
+import 'rc-slider/assets/index.css'
 import styles from './styles.module.scss'
 
 export function Player() {
+  const { episodeList, currentEpisodeIndex } = useContext(PlayerContext)
+
+  const episode = episodeList[currentEpisodeIndex]
+
   return (
    <div className={styles.playerContainer}>
      <header>
@@ -8,33 +18,54 @@ export function Player() {
        <strong>Playing now</strong>
      </header>
 
-     <div className={styles.emptyPlayer}>
-       <strong>Select one podcast to play</strong>
-     </div>
+      { episode ? (
+        <div className={styles.currentEpisode}>
+          <Image
+            width={592}
+            height={592} 
+            src={episode.thumbnail} 
+            objectFit="cover"
+          />
+          <strong>{episode.title}</strong>
+          <span>{episode.members}</span>
+        </div>
+      ) : (  
+        <div className={styles.emptyPlayer}>
+        <strong>Select one podcast to play</strong>
+        </div>
+      )}
 
-     <footer className={styles.empty}>
+     <footer className={!episode ? styles.empty : ''}>
        <div className={styles.progress}>
          <span>00:00</span>
          <div className={styles.slider}>
-          <div className={styles.emptySlider} />
+          {episode ? (
+            <Slider 
+              trackStyle={{ backgroundColor: '#04d361' }}
+              railStyle={{ backgroundColor: '#9f75ff' }}
+              handleStyle={{ borderColor: '#9f75ff', borderWidth: 4 }}
+            />
+          ) : (
+            <div className={styles.emptySlider} />
+          )}
          </div>
          <span>00:00</span>
        </div>
 
        <div className={styles.buttons}>
-         <button type="button">
+         <button type="button" disabled={!episode}>
           <img src="/shuffle.svg" alt="shuffle"/>
          </button>
-         <button type="button">
+         <button type="button" disabled={!episode}>
           <img src="/play-previous.svg" alt="previous"/>
          </button>
-         <button type="button" className={styles.playButton}>
+         <button type="button" className={styles.playButton} disabled={!episode}>
           <img src="/play.svg" alt="play"/>
          </button>
-         <button type="button">
+         <button type="button" disabled={!episode}>
           <img src="/play-next.svg" alt="next"/>
          </button>
-         <button type="button">
+         <button type="button" disabled={!episode}>
           <img src="/repeat.svg" alt="repeat"/>
          </button>
        </div>
--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -26,6 +26,22 @@ type EpisodeProps = {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 2,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const paths = data.map(episode => {
+    return{
+      params: {
+        slug: episode.id
+      }
+    }
+  })
+
   return{
     paths: [],
     fallback: 'blocking'
@@ -33,7 +49,6 @@ export const getStaticPaths: GetStaticPaths = async () => {
 }
 
 export default function Episode({ episode }: EpisodeProps) {
-  const router = useRouter()
   return(
     <div className={styles.episode}>
       <div className={styles.thumbnailContainer}>
